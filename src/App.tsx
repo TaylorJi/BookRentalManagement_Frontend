91dc2081a281b7bd8aab17b506f5b63e07ebc3c3
TaylorJi <sji24@my.bcit.ca>
2024-05-07 23:20:55 -0700

    created index page that has five buttons at the center

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,65 @@
 // import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route, Link as RouterLink } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link as RouterLink,
+} from "react-router-dom";
 
-import { AppBar, Toolbar, Button, Typography, CssBaseline } from '@mui/material';
-import CustomerPage from './pages/customer/customerPage';
-import BookPage from './pages/book/bookPage';
-import TypePage from './pages/type/typePage';
-import RentPage from './pages/bookRent/bookRentpage';
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  CssBaseline,
+} from "@mui/material";
+import CustomerPage from "./pages/customer/customerPage";
+import BookPage from "./pages/book/bookPage";
+import TypePage from "./pages/type/typePage";
+import RentPage from "./pages/bookRent/bookRentpage";
+import IndexPage from "./pages/index";
 
-import './App.css';
-
+import "./App.css";
 
 function App() {
   return (
     <Router>
-    <CssBaseline />  {/* Normalize the styles */}
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Book Rental System
-        </Typography>
-        <Button color="inherit" component={RouterLink} to="/customers">Customers</Button>
-        <Button color="inherit" component={RouterLink} to="/books">Books</Button>
-        <Button color="inherit" component={RouterLink} to="/types">Types</Button>
-        <Button color="inherit" component={RouterLink} to="/rentals">Rentals</Button>
-        {/* Add more navigation buttons as needed */}
-      </Toolbar>
-    </AppBar>
-
-    {/* Define routes for your application */}
-    <Routes>
-      <Route path="/customers" element={<CustomerPage />} />
-      <Route path="/books" element={<BookPage />} />
-      <Route path="/types" element={<TypePage />} />
-      <Route path="/rentals" element={<RentPage />} />
-      {/* Define other routes as needed */}
-    </Routes>
-  </Router>
+      <CssBaseline /> {/* Normalize the styles */}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component={RouterLink}
+            to="/"
+            sx={{ flexGrow: 1, color: "inherit", textDecoration: "none" }}
+          >
+            Book Rental System
+          </Typography>
+          <Button color="inherit" component={RouterLink} to="/customers">
+            Customers
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/books">
+            Books
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/types">
+            Types
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/rentals">
+            Rentals
+          </Button>
+          {/* Add more navigation buttons as needed */}
+        </Toolbar>
+      </AppBar>
+      {/* Define routes for your application */}
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/customers" element={<CustomerPage />} />
+        <Route path="/books" element={<BookPage />} />
+        <Route path="/types" element={<TypePage />} />
+        <Route path="/rentals" element={<RentPage />} />
+        {/* Define other routes as needed */}
+      </Routes>
+    </Router>
     // <Router>
     //   <div>
     //     <nav>
@@ -50,22 +74,18 @@ function App() {
     //       Welcom to Book Rental System
     //     </h1>
 
-
-        
     //     {/* Define routes for your application */}
     //     <Routes>
     //       <Route path="/customers" element={<CustomerPage />} />
     //       <Route path="/books" element={<BookPage />} />
     //       <Route path="/types" element={< TypePage/>} />
     //       <Route path="/rentals" element={< RentPage/>} />
-          
+
     //       {/* Define other routes as needed */}
     //     </Routes>
     //   </div>
     // </Router>
   );
 }
-
-
 
 export default App;
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+function IndexPage() {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="100vh" // This ensures the box takes up at least the full height of the viewport
+    >
+      <Typography variant="h4" gutterBottom>Welcome to the Book Rental System</Typography>
+      <Box>
+        <Button variant="contained" color="primary" component={RouterLink} to="/customers" sx={{ m: 1 }}>
+          Customers
+        </Button>
+        <Button variant="contained" color="primary" component={RouterLink} to="/books" sx={{ m: 1 }}>
+          Books
+        </Button>
+        <Button variant="contained" color="primary" component={RouterLink} to="/rentals" sx={{ m: 1 }}>
+          Rent
+        </Button>
+        <Button variant="contained" color="primary" component={RouterLink} to="/return" sx={{ m: 1 }}>
+          Return
+        </Button>
+        <Button variant="contained" color="primary" component={RouterLink} to="/transactions" sx={{ m: 1 }}>
+          Daily Transactions
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default IndexPage;
