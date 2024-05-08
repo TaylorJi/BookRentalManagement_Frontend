58a805339c3cb74fb05a32ec3f0a13eb1a7a19d0
TaylorJi <sji24@my.bcit.ca>
2024-05-07 23:11:26 -0700

    installed and tested material-ui for frontend framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link as RouterLink } from 'react-router-dom';
+
+import { AppBar, Toolbar, Button, Typography, CssBaseline } from '@mui/material';
 import CustomerPage from './pages/customer/customerPage';
 import BookPage from './pages/book/bookPage';
 import TypePage from './pages/type/typePage';
@@ -10,32 +13,56 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <Link to="/customers">Customers</Link>
-          <Link to="/books">Books</Link>
-          <Link to="/types">Types</Link>
-           <Link to="/rentals">Rentals</Link>
-          {/* Add more links for other navigation items */}
-        </nav>
+    <CssBaseline />  {/* Normalize the styles */}
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Book Rental System
+        </Typography>
+        <Button color="inherit" component={RouterLink} to="/customers">Customers</Button>
+        <Button color="inherit" component={RouterLink} to="/books">Books</Button>
+        <Button color="inherit" component={RouterLink} to="/types">Types</Button>
+        <Button color="inherit" component={RouterLink} to="/rentals">Rentals</Button>
+        {/* Add more navigation buttons as needed */}
+      </Toolbar>
+    </AppBar>
 
-        <h1>
-          Welcom to Book Rental System
-        </h1>
+    {/* Define routes for your application */}
+    <Routes>
+      <Route path="/customers" element={<CustomerPage />} />
+      <Route path="/books" element={<BookPage />} />
+      <Route path="/types" element={<TypePage />} />
+      <Route path="/rentals" element={<RentPage />} />
+      {/* Define other routes as needed */}
+    </Routes>
+  </Router>
+    // <Router>
+    //   <div>
+    //     <nav>
+    //       <Link to="/customers">Customers</Link>
+    //       <Link to="/books">Books</Link>
+    //       <Link to="/types">Types</Link>
+    //        <Link to="/rentals">Rentals</Link>
+    //       {/* Add more links for other navigation items */}
+    //     </nav>
+
+    //     <h1>
+    //       Welcom to Book Rental System
+    //     </h1>
 
 
         
-        {/* Define routes for your application */}
-        <Routes>
-          <Route path="/customers" element={<CustomerPage />} />
-          <Route path="/books" element={<BookPage />} />
-          <Route path="/types" element={< TypePage/>} />
-          <Route path="/rentals" element={< RentPage/>} />
+    //     {/* Define routes for your application */}
+    //     <Routes>
+    //       <Route path="/customers" element={<CustomerPage />} />
+    //       <Route path="/books" element={<BookPage />} />
+    //       <Route path="/types" element={< TypePage/>} />
+    //       <Route path="/rentals" element={< RentPage/>} />
           
-          {/* Define other routes as needed */}
-        </Routes>
-      </div>
-    </Router>
+    //       {/* Define other routes as needed */}
+    //     </Routes>
+    //   </div>
+    // </Router>
   );
 }
 
