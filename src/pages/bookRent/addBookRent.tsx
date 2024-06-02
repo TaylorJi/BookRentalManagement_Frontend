1e8d19eab103e214054c70201291494907a1f624
TaylorJi <sji24@my.bcit.ca>
2024-06-02 16:57:46 -0700

    replaced  the book rent endpoint with the hosted backend

--- src/pages/bookRent/addBookRent.tsx
+++ src/pages/bookRent/addBookRent.tsx
@@ -203,7 +203,7 @@ const BookRentalForm: React.FC<AddBookRentProps> = () => {
       .filter(Boolean); // filter out null values
 
     try {
-      const response = await axios.post("/api/bookRents", {
+      const response = await axios.post(`${apiUrl}/bookRents`, {
         customer_ID: selectedCustomerId,
         books: booksToRent,
       });
